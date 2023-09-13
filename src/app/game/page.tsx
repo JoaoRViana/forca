8ab823dcbe23caf35cee31f44b635456cad4d806
JoaoRViana <joaorviana98@gmail.com>
2023-09-13 13:27:49 -0300

    criando input

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,15 +1,49 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import { getWord } from "./helpers";
 
-export default async function Game(){
-
-    const getAndSetWord = async():Promise<String>=>{
-        const string = await getWord()
-        return string
+export default function Game(){
+    const [word,setWord] = useState<Array<string>>([])
+    const getAndSetWord = async():Promise<void>=>{
+        const string =(await getWord()).split('')
+        setWord(string)
     }
-    const word = await getAndSetWord()
+    useEffect(()=>{
+        getAndSetWord()
+    },[])
+    const verifyLetter = (e:string) =>{
+        console.log(e)
+        console.log(word)
+        if(word.join('').includes(e)){
+            const index = word.indexOf(e);
+            const letter = document.getElementById(`letter${index}`)
+            if(letter){
+                letter.innerHTML = e
+            }
+        }
+    }
     return(
-        <div className="bg-slate-400 min-h-screen">
-            <h2>{word}</h2>
+        <div className="bg-slate-400 min-h-screen" onMouseOver={(e)=>{
+            const input = document.getElementById('forcaInput')
+            input?.focus()
+        }}>
+            <div>
+            <input className="text-black" id="forcaInput" maxLength={1} onChange={(e)=>{
+                verifyLetter(e.target.value)
+                console.log(e.target.value)
+                const input = document.getElementById('forcaInput')
+                if(input){
+                    input.value =''
+                }
+            }}></input>
+            </div>
+
+            <div className="flex flex-wrap">
+                {word.map((e,i)=>(
+                    <h2 key={i} id={`letter${i}`} className="text-2xl mx-2">_</h2>
+                ))}
+            </div>
         </div>
     )
 }
