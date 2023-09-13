4adba25cdc30fb25cc2986509366debd47584b24
JoaoRViana <joaorviana98@gmail.com>
2023-09-13 14:31:20 -0300

    criando vitoria e derrota

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -8,6 +8,8 @@ export default function Game(){
     const [word,setWord] = useState<Array<string>>([])
     const [correctLetters,setCorrectLetters] = useState<Array<string>>([])
     const [victory,setVictory] = useState<boolean>(false)
+    const [loose,setLoose] = useState<boolean>(false)
+    const [attempts,setAttempts] = useState<number>(7)
     const getAndSetWord = async():Promise<void>=>{
         const string =(await getWord()).split('')
         setWord(string)
@@ -24,8 +26,7 @@ export default function Game(){
         const normalizedWord = word.map(letter =>
             letter.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
           );
-         console.log(normalizedWord)
-         console.log(normalizedWord.join('').includes(e))
+
         if(normalizedWord.join('').includes(e)){
             normalizedWord.forEach((l,i)=>{
                 if(l===e){
@@ -41,26 +42,44 @@ export default function Game(){
             if(word.join('') === correctLetters.join('')){
                 setVictory(true)
             }
+        }else{
+            const currentAttempts = attempts-1
+            if(currentAttempts<1){
+                console.log('a')
+                setLoose(true)
+            }
+            setAttempts((attempts-1))
         }
-        
+
     }
     return(
         <div className="bg-slate-400 min-h-screen" onMouseOver={(e)=>{
             const input = document.getElementById('forcaInput')
             input?.focus()
-        }}><div>
-            {victory?<Link href={'/'}>Home</Link>:''}
-        </div>
+        }}>
+            <h2>
+                Tentivas Restantes:{attempts}
+            </h2>
             <div>
-            <input className="text-black" id="forcaInput" maxLength={1} onChange={(e)=>{
+            {victory?<div>
+                <Link href={'/'}>Home</Link>
+                <h2>Parabéns você ganhou!!</h2>
+            </div>:''}
+        </div>
+            {loose?<div>
+                <Link href={'/'}>Home</Link>
+                <div>
+                    <h2>Você perdeu a palavra era {word}</h2>
+                </div>
+            </div>:  <div>
+            <input className="text-black w-0" id="forcaInput" maxLength={1} onChange={(e)=>{
                 verifyLetter(e.target.value)
                 const input = document.getElementById('forcaInput')
                 if(input){
                     input.value =''
                 }
             }}></input>
-            </div>
-
+            </div>}
             <div className="flex flex-wrap">
                 {word.map((e,i)=>(
                     <h2 key={i} id={`letter${i}`} className="text-2xl mx-2">_</h2>
