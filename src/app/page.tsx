7f9aa3c573115844044e564420f72e8c8635250c
JoaoRViana <joaorviana98@gmail.com>
2023-09-14 13:16:04 -0300

    centralizando botão de play

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   const theme = useAppSelector((state)=> state.changeReducer.value)
 
   return (
-    <div className={`flex flex-col items-center ${theme.background} min-h-screen  z-1`}>
-      <div className='self-center my-36'>
+    <div className={`flex flex-col items-center ${theme.background} min-h-screen justify-center  z-1`}>
+      <div className='self-center '>
         <Link href={'/game'}>
         <button className={`text-2xl  bg-blue-500 hover:bg-blue-400 ${theme.textGame} font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded`}>
             Jogar
